34b82111d49337d420b121570405bf6bf8ca8d01
megan <megand0905@gmail.com>
2025-08-19 19:44:59 -0700

    changes to modal content in photography

--- pages/photography/script.js
+++ pages/photography/script.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     // ============ modal logic ============ //
-    // Get all buttons that open modals
     const openModalButtons = document.querySelectorAll('.open-modal-btn');
 
+    // open modal
     openModalButtons.forEach(button => {
         button.addEventListener('click', () => {
             const modalId = button.dataset.modalId; // Get the ID from the data attribute
             const modal = document.getElementById(modalId); // Find the corresponding modal
 
             if (modal) {
-                modal.style.display = 'block'; // Show the modal
+                modal.style.display = 'flex'; // Show the modal
             }
         });
     });
 
-    // Logic for closing modals (e.g., close button or click outside)
+    // close modal on close button click
     const closeButtons = document.querySelectorAll('.close-modal-btn'); // Assuming close buttons have this class
 
     closeButtons.forEach(closeBtn => {
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    // Close modal when clicking outside
+    // close modal on outside content click
     window.addEventListener('click', (event) => {
         const modals = document.querySelectorAll('.modal');
         modals.forEach(modal => {
